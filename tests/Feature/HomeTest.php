5f550cb266a9b142dc7d6d5d2d985944092f8197
Hector Rubio <hector.rubio.920@hotmail.com>
2021-02-22 14:28:07 -0700

    test tags config factory migrate TAG

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Tag;
 
 class HomeTest extends TestCase
 {
@@ -15,5 +16,14 @@ class HomeTest extends TestCase
         ->get('/')
         ->assertStatus(200)
         ->assertSee('No hay etiquetas');
+    }
+    public function testWithData()
+    {
+       $tag = Tag::factory()->create(); 
+       $this
+        ->get('/')
+        ->assertStatus(200)
+        ->assertSee($tag->name)
+        ->assertDontSee('No hay etiquetas');
     }
 }
